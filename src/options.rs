7c4724b88e498e34ac5b16ddf7b2486e725bddbd
Spyros Roum <spyrosr1@gmail.com>
2021-01-23 13:40:05 +0200

    Update some help messages

--- src/options.rs
+++ src/options.rs
@@ -9,16 +9,15 @@ pub struct Opt {
     #[structopt(short, long, parse(from_os_str), default_value = "./pastes/")]
     pub output: PathBuf,
     #[cfg(feature = "web")]
-    /// This will be used as a prefix for an output received by the client.
+    /// This will be used as to construct the url that is returned to the client.
     /// Value will be prepended with `http`.
     #[structopt(short, long, default_value = "localhost")]
     pub domain: String,
     #[cfg(feature = "web")]
     /// If set, returns the url with `https` prefix instead of `http`.
-    #[structopt(short, long)]
+    #[structopt(long)]
     pub https: bool,
-    /// This parameter defines size of the buffer used for getting data from the user.
-    /// Maximum size (in bytes) of all input files is defined by this value.
+    /// Defines the maximum size (in bytes) of the buffer used for getting data from the user.
     #[structopt(short = "B", long, default_value = "50000")]
     pub buffer_size: usize,
 }

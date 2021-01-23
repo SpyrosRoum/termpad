f45b2046814a64d0d16bfc774c190d2899e2b709
Spyros Roum <spyrosr1@gmail.com>
2021-01-23 20:02:12 +0200

    Improve logs

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod utils;
 mod web;
 
 use std::{
-    fs::{create_dir_all, File},
+    fs::{self, File},
     io::{Read, Write},
     net::{TcpListener, TcpStream},
     sync::Arc,
@@ -12,14 +12,18 @@ use std::{
 };
 
 use anyhow::{bail, Context, Result};
+use log::{error, info, warn};
+use simplelog::{Config, LevelFilter, TermLogger, TerminalMode};
 use structopt::StructOpt;
 
 use options::Opt;
 
 fn main() -> Result<()> {
+    TermLogger::init(LevelFilter::Info, Config::default(), TerminalMode::Mixed)
+        .context("Failed to initialise logger.")?;
     let opt: Arc<Opt> = Arc::new(Opt::from_args());
 
-    create_dir_all(&opt.output).context(format!(
+    fs::create_dir_all(&opt.output).context(format!(
         "Failed to create directory `{}`",
         opt.output.display()
     ))?;
@@ -29,16 +33,22 @@ fn main() -> Result<()> {
         }
         _ => {}
     }
+    info!("Using `{}` for saving files", &opt.output.display());
 
     // This thread listens to port 9999 for accepting input and creating new files.
     let input_thread = {
-        let listener = TcpListener::bind(("0.0.0.0", 9999))?;
+        let listener = TcpListener::bind(("0.0.0.0", 9999))
+            .context("Failed to bind to port 9999 for input listener")?;
         let opt = Arc::clone(&opt);
         thread::Builder::new()
             .name(String::from("Input thread"))
             .spawn(move || {
                 for stream in listener.incoming() {
                     if stream.is_err() {
+                        warn!(
+                            "Could not connect with client for input {:?}",
+                            stream.err().unwrap()
+                        );
                         continue;
                     }
                     let stream = stream.unwrap();
@@ -48,17 +58,22 @@ fn main() -> Result<()> {
             })
             .context("Failed to start Input thread")?
     };
-    println!("Listening to port 9999 for input");
+    info!("Listening to port 9999 for input");
 
     // This thread listens to port 8888 for accepting file names and returning their content.
     let output_thread = {
-        let listener = TcpListener::bind(("0.0.0.0", 8888))?;
+        let listener = TcpListener::bind(("0.0.0.0", 8888))
+            .context("Failed to bind to port 8888 for output listener")?;
         let opt = Arc::clone(&opt);
         thread::Builder::new()
             .name(String::from("Output thread"))
             .spawn(move || {
                 for stream in listener.incoming() {
                     if stream.is_err() {
+                        warn!(
+                            "Could not connect with client for output {:?}",
+                            stream.err().unwrap()
+                        );
                         continue;
                     }
                     let stream = stream.unwrap();
@@ -68,7 +83,7 @@ fn main() -> Result<()> {
             })
             .context("Failed to start Output thread")?
     };
-    println!("Listening to port 8888 for output");
+    info!("Listening to port 8888 for output");
 
     #[cfg(feature = "web")]
     {
@@ -77,7 +92,6 @@ fn main() -> Result<()> {
             .name(String::from("Web thread"))
             .spawn(move || web::web_main(opt))
             .context("Failed to start Web thread")?;
-        println!("Web server running in port 3030");
     }
     println!("====================");
 
@@ -90,33 +104,45 @@ fn handle_client(stream: TcpStream, settings: Arc<Opt>, input: bool) {
     let addr = stream.peer_addr();
     let ip = addr.map_or(String::from("n/a"), |addr| addr.to_string());
 
-    let direction = if input {
+    if input {
+        info!("Getting input from {}", ip);
         handle_client_input(stream, settings);
-        "Getting input from"
     } else {
+        info!("Sending output to {}", ip);
         handle_client_output(stream, settings);
-        "Sending output to"
     };
-
-    println!("{} {}", direction, ip);
 }
 
 fn handle_client_input(mut stream: TcpStream, settings: Arc<Opt>) {
     let mut buffer = vec![0; settings.buffer_size];
 
-    let read = stream.read(&mut buffer).unwrap();
+    let read = stream.read(&mut buffer);
+    if read.is_err() {
+        error!("Could not read data from input stream.");
+        return;
+    }
+    let read = read.unwrap();
 
-    let mut file = settings.output.clone();
+    let mut file_path = settings.output.clone();
     let name = loop {
         let name = utils::gen_name();
-        file.push(name.to_lowercase());
-        if !file.is_file() {
+        file_path.push(name.to_lowercase());
+        if !file_path.is_file() {
             break name;
         }
         // Since the file exists we remove the last part so we can get a new one on the next iteration
-        file.pop();
+        file_path.pop();
     };
-    let mut file = File::create(file).unwrap();
+    let file = File::create(&file_path);
+    if file.is_err() {
+        error!(
+            "Could not create `{}`: {:?}",
+            file_path.display(),
+            file.err().unwrap()
+        );
+        return;
+    }
+    let mut file = file.unwrap();
     file.write_all(&buffer[..read]).unwrap();
 
     // The answer is usually printed on the other side so we just add a newline here
@@ -128,22 +154,41 @@ fn handle_client_input(mut stream: TcpStream, settings: Arc<Opt>) {
     }
     answer.push_str(name.as_str());
     answer.push('\n');
-    stream.write_all(answer.as_bytes()).unwrap();
+
+    stream
+        .write_all(answer.as_bytes())
+        .map_err(|e| error!("Could not send reply to input request: {:?}", e))
+        .ok();
 }
 
 fn handle_client_output(mut stream: TcpStream, settings: Arc<Opt>) {
     let mut buffer = [0; 50];
 
-    let read = stream.read(&mut buffer).unwrap();
+    let read = stream.read(&mut buffer);
+    if read.is_err() {
+        error!("Could not read data from output stream.");
+        return;
+    }
+    let read = read.unwrap();
 
     // We do `read - 1` to account for the `\n`
     let file_name = String::from_utf8_lossy(&buffer[..read - 1]).to_lowercase();
 
     let file_path = settings.output.join(&file_name);
     if file_path.is_file() {
-        let mut file = File::open(file_path).unwrap();
-        let mut content = String::new();
-        file.read_to_string(&mut content).unwrap();
-        stream.write_all(content.as_bytes()).unwrap();
+        let content = fs::read_to_string(&file_path);
+        if content.is_err() {
+            error!(
+                "Could not read content from `{}`: {:?}",
+                file_path.display(),
+                content.err().unwrap()
+            );
+            return;
+        }
+        let content = content.unwrap();
+        stream
+            .write_all(content.as_bytes())
+            .map_err(|e| error!("Could not send reply to output request: {:?}", e))
+            .ok();
     }
 }

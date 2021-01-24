32ca256db79cdceae10fdc1897d298a741bbc267
Spyros Roum <spyrosr1@gmail.com>
2021-01-24 04:27:23 +0200

    Remove `output` env variable and change `DOMAIN` to `DOMAIN_NAME`
    Env variables are needed for docker, in docker we don't care about the output folder

--- src/options.rs
+++ src/options.rs
@@ -6,17 +6,11 @@ use structopt::StructOpt;
 #[structopt(name = "termpad")]
 pub struct Opt {
     /// Relative or absolute path to the directory where you want to store user-posted pastes.
-    #[structopt(
-        short,
-        long,
-        parse(from_os_str),
-        default_value = "./pastes/",
-        env = "OUTPUT"
-    )]
+    #[structopt(short, long, parse(from_os_str), default_value = "./pastes/")]
     pub output: PathBuf,
     /// This will be used as to construct the url that is returned to the client.
     /// Value will be prepended with `http`.
-    #[structopt(short, long, default_value = "localhost", env = "DOMAIN")]
+    #[structopt(short, long, default_value = "localhost", env = "DOMAIN_NAME")]
     pub domain: String,
     /// If set, returns the url with `https` prefix instead of `http`.
     #[structopt(long, env = "HTTPS")]
